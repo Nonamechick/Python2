996a2aeaed35eb74ad7c89bdb95a8fa67414b1b6
Nonamechick <otajonovxasan350@gmail.com>
2025-02-04 16:00:58 +0500

    3 ex

--- first.py
+++ first.py
@@ -19,3 +19,29 @@ def calculate(a,b):
 s,p = calculate(4,5)
 print("sum",s)
 print("product",p)
+
+def cubeVolume(sideLength):
+    volume = sideLength ** 3
+    return volume
+res1 = cubeVolume(2)
+res2 = cubeVolume(10)
+print("res1",res1)
+print("res2",res2)
+
+def recrangleArea(w, l):
+    return w*l
+print(recrangleArea(4,5))
+
+
+def is_even(check):
+    if check % 2 == 0:
+        return "It's even"
+
+print(is_even(10))
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(6))  
