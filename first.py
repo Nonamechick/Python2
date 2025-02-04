9f539674cfdc2ccc0897deb9e435ed7e9d6c7b16
Nonamechick <otajonovxasan350@gmail.com>
2025-02-04 15:40:00 +0500

    example

---
+++ first.py
@@ -0,0 +1,13 @@
+def greet():
+    print("No hello")
+greet()
+
+def add(a,b):
+    return a+b
+print(add(5,3))
+
+def power(base, exponent=2):
+    return base ** exponent
+
+print(power(3))
+print(power(3,3))
