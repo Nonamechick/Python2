76bd7946a16200dbe751a27ac2db044ff37a8b13
Nonamechick <otajonovxasan350@gmail.com>
2025-02-04 15:44:23 +0500

    second ex

--- first.py
+++ first.py
@@ -11,3 +11,11 @@ def power(base, exponent=2):
 
 print(power(3))
 print(power(3,3))
+
+def calculate(a,b):
+    sum_res = a+b
+    product = a*b
+    return sum_res, product
+s,p = calculate(4,5)
+print("sum",s)
+print("product",p)
