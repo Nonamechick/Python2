0f0602dd510b3ebd5408a6c2accc883c8810f980
Nonamechick <otajonovxasan350@gmail.com>
2025-02-07 12:28:05 +0500

    4

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 import math
-
-
+from functools import reduce
+"""
 def greet():
     print("No hello")
 greet()
@@ -67,3 +67,51 @@ def mystery(x,y):
     res = (x+y)/(y-x)
     return res
 print(mystery(2,3))
+
+numbers = [1, 2, 3, 4]
+squared = list(map(lambda x: x**2, numbers))
+print(squared)  
+
+number1 = [1, 2, 3, 4, 5, 6]
+even_numbers = list(filter(lambda x: x % 2 == 0, number1))
+print(even_numbers)
+
+product = reduce(lambda x,y: x*y,numbers)
+print(product)
+
+pairs = [(1,2),(4,1),(3,5)]
+sorted_pairs = sorted(pairs, key=lambda x: x[1])
+print(sorted_pairs)
+"""
+
+#ex 
+students = [
+    {"name": "Alice", "score": 88, "age": 20},
+    {"name": "Bob", "score": 92, "age": 22},
+    {"name": "Charlie", "score": 88, "age": 19},
+    {"name": "David", "score": 95, "age": 21}
+]
+
+sorted_students = sorted(students, key=lambda x: (-x["score"], x["age"]))
+print(sorted_students)
+
+
+words = ['apple', 'banana','cherry','blueberry','strawberry']
+
+word = reduce(lambda x, y: x if len(x) > len(y) else y, words)
+print(word)
+
+
+num = [10,17,19,21,22,23,29,30,31]
+
+prime = lambda n: n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
+
+prime_numbers = list(filter(prime, num))
+
+print(prime_numbers)
+
+words1 = ['hi','hello','to','world', 'a','python']
+
+modif = list(map(lambda word: word.upper() if len(word) > 3 else word, words1))
+
+print(modif) 
