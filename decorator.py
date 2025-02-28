c7753d538e3bd1a84fd417badbfddc206f2ba6ad
Nonamechick <otajonovxasan350@gmail.com>
2025-02-28 11:47:10 +0500

    11

--- decorator.py
+++ decorator.py
@@ -8,3 +8,13 @@ def my_decorator(func):
 def hello():
     print('Hell, World')
 hello()
+
+def decorator_function(original_function):
+    def wrapper_function():
+        print('Wrapper executed this before {}'. format(original_function.__name__))
+        return original_function()
+    return wrapper_function
+@decorator_function
+def display():
+    print('Display function executed')
+display()
