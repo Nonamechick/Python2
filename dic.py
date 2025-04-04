61c6f0e3b7f4580e01638d058db172533540cc1b
Nonamechick <otajonovxasan350@gmail.com>
2025-04-04 11:47:17 +0500

    28

--- dic.py
+++ dic.py
@@ -3,3 +3,10 @@ car_dict = {'a': 'Mercedes-Benz','b': 'BWM', 'c': 'Ferrari', 'd': 'Lamborghini',
 car_dict = dict(map(lambda x: (x[0], x[1]+'_'), car_dict.items() ))
 print('The modified dictionary is : ')
 print(car_dict)
+
+def example(i):
+    return i%3
+set_exm = {33,102,62,96,44,28,227}
+upd_itm = map(example,set_exm)
+print(upd_itm)
+print(set(upd_itm))
