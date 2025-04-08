46c2e734cfd63f0047bc043e880b6c65e26b26a9
nonamechick <nonamechick7@gmail.com>
2025-04-08 15:55:50 +0500

    32

---
+++ nump.py
@@ -0,0 +1,19 @@
+import numpy as np
+arr = np.array([1,2,3,4,5])
+a=np.array([1,2,3,4,5,6])
+print(a,arr)
+print(type(a))
+
+d = np.array((1,2,3,4,5))
+print(d)
+
+print(np.empty(3))
+print(np.array([3.14,42.]))
+print(np.arange(5))
+print(np.array([0,1,2,3]))
+print(np.arange(2,9,2))
+print(np.array([2,4,6,8]))
+print(np.linspace(0,10,num=5))
+print(np.array([0.,2.5,5.,7.5,10.]))
+o = np.array([2,4,42,5,1,55,312,3131,78])
+print(np.sort(arr))
